1bd862a279392b92ebf66c51257705b09c670055
Edvard <edvardfauchelevent@gmail.com>
2025-01-10 16:59:11 +0000

    First commit

---
+++ src/main/java/com/onesquad/formulafan/FormulaFanBackendApplication.java
@@ -0,0 +1,11 @@
+package com.onesquad.formulafan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormulaFanBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FormulaFanBackendApplication.class, args);
+    }
+}
